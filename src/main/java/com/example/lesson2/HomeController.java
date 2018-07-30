36cc31da39de1f157f753ee6d1f20c99671d0f04
GBTC441012UR <GBTC441012UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-30 17:06:57 -0400

    learn2

---
+++ src/main/java/com/example/lesson2/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.lesson2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public  String homePage(Model model)
+    {
+        model.addAttribute("myvar" , "Say Hello to the people.");
+        return "hometemplate";
+    }
+
+}
